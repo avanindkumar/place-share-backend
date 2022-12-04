3d530315b9ceab35b35c4e5d81a769c7e148febd
Avanind Parjapati <avanindkumar@gmail.com>
2022-12-04 16:17:34 +0530

    Update s3Service.js

--- util/s3Service.js
+++ util/s3Service.js
@@ -1,22 +1,19 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
+const {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
 
 exports.s3Upload = async (file, fileName) => {
-  return s3
-    .putObject({
-      Body: file.buffer,
-      Bucket: "cyclic-worried-tux-pig-ap-northeast-2",
-      Key: fileName,
-    })
-    .promise();
-  // const REGION = process.env.AWS_BUCKET_REGION;
-  // const s3 = new S3Client({ region: REGION });
-  // const param = {
-  //   Bucket: process.env.AWS_BUCKET_NAME,
-  //   Key: fileName,
-  //   Body: file.buffer,
-  // };
-  // return s3.send(new PutObjectCommand(param));
+  const REGION = process.env.AWS_BUCKET_REGION;
+  const s3 = new S3Client({ region: REGION });
+
+  const param = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+    Body: file.buffer,
+  };
+  return s3.send(new PutObjectCommand(param));
 };
 // exports.s3Delete = async (fileName) => {
 //   const REGION = process.env.AWS_BUCKET_REGION;
