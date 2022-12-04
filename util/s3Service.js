0da7227099630c05d5e1b617c222e870cbfe8023
Avanind Parjapati <avanindkumar@gmail.com>
2022-12-04 16:12:07 +0530

    temp changes

--- util/s3Service.js
+++ util/s3Service.js
@@ -5,15 +5,21 @@ const {
 } = require("@aws-sdk/client-s3");
 
 exports.s3Upload = async (file, fileName) => {
-  const REGION = process.env.AWS_BUCKET_REGION;
-  const s3 = new S3Client({ region: REGION });
-
-  const param = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: fileName,
-    Body: file.buffer,
-  };
-  return s3.send(new PutObjectCommand(param));
+  return s3
+    .putObject({
+      Body: file.buffer,
+      Bucket: "cyclic-worried-tux-pig-ap-northeast-2",
+      Key: fileName,
+    })
+    .promise();
+  // const REGION = process.env.AWS_BUCKET_REGION;
+  // const s3 = new S3Client({ region: REGION });
+  // const param = {
+  //   Bucket: process.env.AWS_BUCKET_NAME,
+  //   Key: fileName,
+  //   Body: file.buffer,
+  // };
+  // return s3.send(new PutObjectCommand(param));
 };
 // exports.s3Delete = async (fileName) => {
 //   const REGION = process.env.AWS_BUCKET_REGION;
